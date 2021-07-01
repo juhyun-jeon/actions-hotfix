bc8a0291ab35937ca7ca439285b4331741d84aaf
juhyun-jeon <wjswngus1012@gmail.com>
2021-07-01 14:41:59 +0900

    ?!

--- actions.sh
+++ actions.sh
@@ -2,7 +2,7 @@
 git remote update && \
 git fetch --all && \
 git checkout -b "$PR_BRANCH" origin/main && \
-echo "$(git merge-base "$PR_BRANCH" prod)" && \ 
+echo "$(git merge-base $PR_BRANCH prod)" && \ 
 git cherry-pick $(git merge-base "$PR_BRANCH" prod)..prod && \
 git push -u origin "$PR_BRANCH" && \
 git checkout main
