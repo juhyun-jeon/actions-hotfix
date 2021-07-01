f9c2a5bbe041572c3b15c3fb7c8c63f5c7be4211
juhyun-jeon <wjswngus1012@gmail.com>
2021-07-01 14:56:57 +0900

    add

--- actions.sh
+++ actions.sh
@@ -6,7 +6,7 @@ set -u
 git remote update && \
 git fetch --all && \
 git checkout -b "$PR_BRANCH" origin/main && \
-git cherry-pick ..prod && \
+git cherry-pick $(git merge-base "$PR_BRANCH" prod)..prod && \
 git push -u origin "$PR_BRANCH" && \
 git checkout main
 echo "HI"
