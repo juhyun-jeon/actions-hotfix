587ddb78750cfce67fedc9372baef57a46981eec
juhyun-jeon <wjswngus1012@gmail.com>
2021-07-01 15:00:01 +0900

    hi

--- actions.sh
+++ actions.sh
@@ -6,7 +6,7 @@ set -u
 git remote update && \
 git fetch --all && \
 git checkout -b "$PR_BRANCH" origin/main && \
-git cherry-pick $(git merge-base origin/main prod)..prod && \
+git cherry-pick db6b513..prod && \
 git push -u origin "$PR_BRANCH" && \
 git checkout main
 echo "HI"
