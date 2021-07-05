4b89eec85d22c355ecffd9b96c68ff0f5e9b7b65
sjquant <seonujang92@gmail.com>
2021-07-06 00:17:56 +0900

    test2107161217

--- actions.sh
+++ actions.sh
@@ -7,6 +7,6 @@ git remote update && \
 git fetch --all && \
 git checkout main && git reset --hard && \
 git checkout -b "$PR_BRANCH" origin/main && \
-git cherry-pick PR_BRANCH..prod  && \
+git cherry-pick "$PR_BRANCH"..prod  && \
 git push -u origin "$PR_BRANCH" && \
 git checkout main
