5a078599185d5d984a1ee3329c1385701235a48a
sjquant <seonujang92@gmail.com>
2021-07-05 23:37:39 +0900

    test2107252337

--- actions.sh
+++ actions.sh
@@ -6,7 +6,7 @@ set -u
 git remote update && \
 git fetch --all && \
 git checkout -b "$PR_BRANCH" origin/main && \
-git cherry-pick prod..${{github.sha}} && \
+git cherry-pick prod.."${{github.sha}}" && \
 git push -u origin "$PR_BRANCH" && \
 git checkout main
 echo "HI"
