0c8c87b081d58064bfc9aa57f45d404829f3e270
sjquant <seonujang92@gmail.com>
2021-07-06 00:26:35 +0900

    test210706

--- actions.sh
+++ actions.sh
@@ -5,7 +5,7 @@ set -u
 
 git remote update && \
 git fetch --all && \
-git checkout main && git reset --hard && \
+git checkout main && git cherry-pick --abort || git reset --hard && \
 git checkout -b "$PR_BRANCH" origin/main && \
 git cherry-pick main..prod || git add . && git commit -m "hello" && \
 git push -u origin "$PR_BRANCH" && \
