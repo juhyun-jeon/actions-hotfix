0cde65dbc020e3c1e646a6e4813fc3ffab3bf301
sjquant <seonujang92@gmail.com>
2021-07-05 23:36:29 +0900

    test2107252336

--- actions.sh
+++ actions.sh
@@ -6,7 +6,7 @@ set -u
 git remote update && \
 git fetch --all && \
 git checkout -b "$PR_BRANCH" origin/main && \
-git cherry-pick db6b513..prod && \
+git cherry-pick prod..${{github.sha}} && \
 git push -u origin "$PR_BRANCH" && \
 git checkout main
 echo "HI"
