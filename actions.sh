3ca6e370873a5d12e826c61d76e25cc598ab4698
sjquant <seonujang92@gmail.com>
2021-07-05 23:56:08 +0900

    test2107052355

--- actions.sh
+++ actions.sh
@@ -5,7 +5,7 @@ set -u
 
 git remote update && \
 git fetch --all && \
-git checkout -b "$PR_BRANCH" main && \
-git rebase "$PR_BRANCH" prod && \
+git checkout -b "$PR_BRANCH" origin/main && \
+git rebase prod "$PR_BRANCH" && \
 git push -u origin "$PR_BRANCH" && \
 git checkout main
